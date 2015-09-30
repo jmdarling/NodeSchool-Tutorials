71c7c21ca13d7b34d764297b6f2c51db93550314
Jonathan Darling <jmdarling@me.com>
2015-09-29 23:03:07 -0500

    Completes exercise 1 of 11 - HELLOW_REACT

---
+++ learnyoureact/program.js
@@ -0,0 +1,16 @@
+// The code above creates a small Express server that renders our React components. If someone navigates to /, views/index.jsx will be rendered. This program uses the express-react-views module.
+var express = require('express');
+var app = express();
+
+app.set('port', (process.argv[2] || 3000));
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine());
+
+require('node-jsx').install();
+
+app.use('/', function (req, res) {
+  res.render('index', '');
+});
+
+app.listen(app.get('port'), function () {});
